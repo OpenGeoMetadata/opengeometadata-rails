72d15ba33f9d9bf9625020281a2a29fb42cf685d
Darren Hardy <drh@stanford.edu>
2014-12-02 12:41:55 -0800

    update datadir

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,10 +30,10 @@ set :deploy_to, '/opt/app/geostaff/opengeometadata-rails'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.datadir = '/var/cache/opengeometadata'
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
